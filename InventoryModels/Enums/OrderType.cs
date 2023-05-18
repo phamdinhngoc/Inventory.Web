7fd99c71d06eb97c640ecd083b2912b888dc307e
unknown <phamdinhngoc86@gmail.com>
2023-05-18 16:31:40 +0700

    first commit

---
+++ InventoryModels/Enums/OrderType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryModels.Enums
+{
+    public enum OrderType
+    {
+        PurchaseOrder,
+        CustomerOrder
+    }
+
+    public enum OrderStatus
+    {
+        CheckOut,
+        Paid,
+        Failed,
+        Shipped,
+        Delivered,
+        Returned,
+        Completed
+    }
+}
---
+++ InventoryModels/Item.cs
@@ -0,0 +1,20 @@
+﻿using InventoryModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryModels
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int AvailQuantity { get; set; }
+        public Status Status { get; set; }
+    }
+}
+
